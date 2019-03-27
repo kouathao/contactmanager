b29feeec100dd3c8c3701dbdc5c44fd3f45a0f01
Koua Thao <kouathao09@gmail.com>
2019-03-26 23:54:07 -0500

    Updates

---
+++ precache-manifest.45cd9923e5d5cc3cde37d9a6824d1659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "42511405c0dad3e9ba2d",
+    "url": "/contactmanager/static/js/main.67c51c3b.chunk.js"
+  },
+  {
+    "revision": "6ecc27185202fa08827c",
+    "url": "/contactmanager/static/js/2.38435115.chunk.js"
+  },
+  {
+    "revision": "42511405c0dad3e9ba2d",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6ecc27185202fa08827c",
+    "url": "/contactmanager/static/css/2.d92e014f.chunk.css"
+  },
+  {
+    "revision": "da1843b074a7130fea7e12da356c7307",
+    "url": "/contactmanager/index.html"
+  }
+];
